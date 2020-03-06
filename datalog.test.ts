9a7974515a420344cbe2c8b1bea6ca4e88f84351
Marco Munizaga <marco@marcopolo.io>
2020-03-06 09:53:46 -0800

    Add extendWith & Leaper

--- datalog.test.ts
+++ datalog.test.ts
@@ -2,31 +2,49 @@ import * as datalog from './datalog'
 
 type PersonID = number
 
-describe('Free Variable', () => {
-    test('Constrains', () => {
-        // { id: PersonID, name: String}
-        const person = datalog.Relation<{ id: PersonID, name: string }>()
-        const v = new datalog.FreeVariable()
-        v.constrain([1, 2, 3, 4])
-        expect(v.availableValues).toStrictEqual([1, 2, 3, 4]);
-        v.constrain([2, 3])
-        expect(v.availableValues).toStrictEqual([2, 3]);
-    });
-})
+// describe('Free Variable', () => {
+//     test('Constrains', () => {
+//         // { id: PersonID, name: String}
+//         const person = datalog.Relation<{ id: PersonID, name: string }>()
+//         const v = new datalog.FreeVariable()
+//         v.constrain([1, 2, 3, 4])
+//         expect(v.availableValues).toStrictEqual([1, 2, 3, 4]);
+//         v.constrain([2, 3])
+//         expect(v.availableValues).toStrictEqual([2, 3]);
+//     });
+// })
 
 describe('Relation', () => {
-    test('Returns a function', () => {
-        // { id: PersonID, name: String}
-        const person = datalog.Relation<{ id: PersonID, name: string }>()
+    // test('Returns a function', () => {
+    // { id: PersonID, name: String}
+    // const person = datalog.Relation<{ id: PersonID, name: string }>()
 
-        expect(typeof person).toBe("function");
+    // expect(typeof person).toBe("function");
+    // });
+    const newPerson = () => new datalog.RelationIndex<"id", PersonID, { name: string }>([], ["id", "name"])
+
+    test('Inserts something in the correct place', () => {
+        // { id: PersonID, name: String}
+        const person = newPerson()
+
+        expect(person.elements).toEqual([])
+        person.assert({ id: 0, name: "marco" })
+        expect(person.elements).toEqual([[0, "marco"]])
+        person.assert({ id: 1, name: "daiyi" })
+        expect(person.elements).toEqual([[0, "marco"], [1, "daiyi"]])
     });
 
-    test('Returns a function', () => {
+    test('ExtendWith & Leaper works', () => {
         // { id: PersonID, name: String}
-        const person = datalog.Relation<{ id: PersonID, name: string }>()
+        const person = newPerson()
+        person.assert({ id: 0, name: "marco" })
+        person.assert({ id: 1, name: "daiyi" })
+        person.assert({ id: 2, name: "beba" })
 
-        expect(typeof person).toBe("function");
+        const personExtendedWith = person.extendWith(([id]: [PersonID]) => id);
+        expect(personExtendedWith.count([0])).toEqual(1)
+        expect(personExtendedWith.propose([0])).toEqual([["marco"]])
+        expect(personExtendedWith.intersect([0], [["marcopolo"], ["marco"]])).toEqual([["marco"]])
     });
 
     // test('Propose returns the item added', () => {
--- datalog.ts
+++ datalog.ts
@@ -1,5 +1,152 @@
 // @ts-ignore
 import * as DataFrog from 'datafrog-js'
+
+type ValueOf<T> = T[keyof T];
+
+type Tupleized<T> = Array<ValueOf<T>>
+
+// type Tupleized<K, Val> = [K, ...Array<ValueOf<Val>>]
+
+interface Leaper<P, V> {
+    count: (prefix: P) => number
+    propose: (prefix: P) => Array<V>
+    // Could be faster if we mutate vals
+    intersect: (prefix: P, vals: Array<V>) => Array<V>
+}
+
+// type ExtendWith = {
+//     keyFunc: (P: P) => K
+//     relation: RelationIndex<KName, K, Val>
+// }
+
+class ExtendWith<P, KName, K, Val> implements Leaper<P, Tupleized<Val>> {
+    keyFunc: (P: P) => K
+    relation: RelationIndex<KName, K, Val>
+
+    startIdx: number = 0
+    endIdx: number = 0
+
+    constructor(keyFunc: (P: P) => K, relation: RelationIndex<KName, K, Val>) {
+        this.keyFunc = keyFunc
+        this.relation = relation
+    }
+
+    count(prefix: P): number {
+        const key = this.keyFunc(prefix)
+        this.startIdx = DataFrog.gallop(this.relation.elements, (row: any) => DataFrog.sortTuple(row[0], key) === -1)
+        this.endIdx = DataFrog.gallop(this.relation.elements, (row: any) => DataFrog.sortTuple(row[0], key) !== 0, this.startIdx + 1)
+        return this.endIdx - this.startIdx
+    }
+
+    propose(prefix: P): Array<Tupleized<Val>> {
+        return this.relation.elements.slice(this.startIdx, this.endIdx).map(([_, ...rest]) => rest)
+    }
+
+    // Could be faster if we mutate vals
+    intersect(prefix: P, vals: Array<Tupleized<Val>>): Array<Tupleized<Val>> {
+        const key = this.keyFunc(prefix)
+        let startIdx = this.startIdx;
+        return vals.filter(val => {
+            startIdx = DataFrog.gallop(this.relation.elements, ([_, ...rest]: any) => DataFrog.sortTuple(rest, val) === -1)
+            if (startIdx >= this.relation.elements.length) {
+                return false
+            }
+
+            return DataFrog.sortTuple(this.relation.elements[startIdx]?.slice(1), val) === 0
+        })
+    }
+}
+
+export class RelationIndex<KName, K, Val> {
+    // Array of tuples
+    elements: Array<[K, ...Array<ValueOf<Val>>]>
+    keyOrdering: [KName, ...Array<keyof Val>]
+
+    constructor(elements: Array<[K, ...Array<ValueOf<Val>>]>, keyOrdering: [KName, ...Array<keyof Val>]) {
+        this.elements = elements
+        this.keyOrdering = keyOrdering
+    }
+
+    indexBy<NewKName extends keyof Val | KName, NewK extends ValueOf<Val> | K, NewVal extends ValueOf<Val> | K>(newkeyOrdering: [NewKName, ...Array<keyof NewVal>]): RelationIndex<NewKName, NewK, NewVal> {
+        const keyMapping = this.keyOrdering.reduce((acc: { [key: string]: number }, k, idx) => {
+            acc[k as string] = idx
+            return acc
+        }, {})
+
+        const newData = this.elements.map(row => newkeyOrdering.map(k => row[keyMapping[k as string]])) as Array<[NewK, ...Array<ValueOf<NewVal>>]>
+        return new RelationIndex(newData, newkeyOrdering)
+    }
+
+    assert(element: any /*{[KName]: K} & Val*/) {
+        // @ts-ignore
+        this.insertRow(this.keyOrdering.map(k => element[k]))
+    }
+
+    insertRow(newRow: [K, ...Array<ValueOf<Val>>]) {
+        const insertIdx = DataFrog.gallop(this.elements, (row: any) => DataFrog.sortTuple(row, newRow) === -1)
+        this.elements.splice(insertIdx, 0, newRow)
+    }
+
+    extendWith<P>(keyFunc: (prefix: P) => K): ExtendWith<P, KName, K, Val> {
+        return new ExtendWith(keyFunc, this)
+    }
+
+    // indexError(prefix: Partial<T>) {
+    //     const key = prefix[this.keyOrdering[0]]
+    //     throw new Error(`Relation is not indexed properly! Expected to by indexed by ${key}, but was only given ${Object.keys(prefix)}`)
+    // }
+
+    // Rough heuristic for now, could be better?
+    // count(prefix: Partial<>): number {
+    //     return this.elements.length
+    // }
+    // propose(prefix: Partial<T>): Array<Tupleized<T>> {
+    //     const key = prefix[this.keyOrdering[0]]
+    //     if (!key) {
+    //         this.indexError(prefix)
+    //         return []
+    //     }
+
+    //     const out = []
+    //     const startIdx = DataFrog.gallop(this.elements, (row: Array<ValueOf<T>>) => DataFrog.sortTuple(row[0], key) === -1)
+    //     const endIdx = DataFrog.gallop(this.elements, (row: Array<ValueOf<T>>) => DataFrog.sortTuple(row[0], key) !== -1, startIdx)
+    //     for (let index = startIdx; index < endIdx; index++) {
+    //         out.push(this.elements[index])
+    //     }
+    //     return []
+    // }
+
+    // // Could be faster if we mutate vals
+    // intersect(prefix: Partial<T>, vals: Array<Tupleized<T>>): Array<Tupleized<T>> {
+    //     const key = prefix[this.keyOrdering[0]]
+    //     if (!key) {
+    //         this.indexError(prefix)
+    //         return []
+    //     }
+    //     const out = []
+    //     let i = 0
+    //     let j = 0
+    //     while (i < vals.length && j < vals.length) {
+    //         const rowA = vals[i]
+    //         const rowB = this.elements[j]
+    //         if (DataFrog.sortTuple(rowA, rowB) === -1) {
+    //             // Advance A
+    //         if (DataFrog.sortTuple(rowA, rowB) === 1) {
+    //             // Advance B
+    //         } else {
+    //             // Same
+    //             i++
+    //             j++
+    //             out.push(rowA)
+    //         }
+
+    //     }
+
+    //     return []
+    // }
+}
+
+
 type FindExpression = (<A>(a: A) => {}) | (<A, B>(a: A, b: B) => {}) | (<A, B, C>(a: A, b: B, c: C) => {}) | (<A, B, C, D>(a: A, b: B, c: C, d: D) => {})
 
 function find(findExpression: FindExpression): any {
@@ -90,14 +237,6 @@ export class FreeVariable<T> {
     }
 }
 
-interface Leaper<P, Val> {
-    <K extends keyof Val>(freeVariableObj: { [key: string]: FreeVariable<Val[K]> }): void;
-    count: (prefix: P) => number
-    propose: (prefix: P) => Array<Val>
-    // Could be faster if we mutate vals
-    intersect: (prefix: P, vals: Array<Val>) => Array<Val>
-}
-
 interface Tell<Val> {
     assert: (v: Val) => void
     retract: (v: Val) => void
@@ -126,6 +265,42 @@ function findOnce(findExpression: FindExpression) {
 export function figureOutHowManyFreevar(findExpression: FindExpression) {
     findExpression
 }
+
+
+// class Relation<T> {
+//     // Array of tuples
+//     indices: { [key: string]: RelationIndex<T> } = {}
+//     firstKey: string | null = null
+//     constraint: Partial<T> = {}
+
+//     keyForIndex(key: Array<keyof T>): string {
+//         return key.join('$')
+//     }
+
+//     indexFor(key: Array<keyof T>) {
+//         if (this.firstKey === null) {
+//             throw new Error("No data")
+//         }
+//         const k = this.keyForIndex(key)
+//         if (this.indices[k]) {
+//             return this.indices[k]
+//         }
+
+//         const newIndex = this.indices[this.firstKey].indexBy(key)
+//         this.indices[k] = newIndex
+//         return newIndex
+//     }
+
+//     assert(datum: T) {
+//         Object.values(this.indices).forEach(relationIndex => {
+//             relationIndex.assert(datum)
+//         })
+//     }
+
+//     next() {
+//         return { done: true }
+//     }
+// }
 
 // interface ThisRelation {
 //     indices: { [idxOrder: string]: DataFrog.Relation }
