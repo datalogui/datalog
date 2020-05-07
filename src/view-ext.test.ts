0f5c260e12c8ff7c72fb6bf766042b5709de4312
Marco Munizaga <git@marcopolo.io>
2020-05-07 08:44:33 -0700

    Add View Extensions

--- src/view-ext.test.ts
+++ src/view-ext.test.ts
@@ -1,6 +1,226 @@
+import * as datalog from './datalog'
+import { Added, Modified, Removed } from './datalog'
+import * as ViewExt from './view-ext'
+
 describe("dummy", () => {
   test("dummy", () => {
     expect(true).toBe(true)
+  })
+})
 
+describe("Map", () => {
+  test("maps simple values", () => {
+    const table = datalog.intoTable([
+      { n: 0 },
+      { n: 1 },
+      { n: 2 },
+    ])
+    const view = table.view()
+    const viewExt = new ViewExt.Impl(view)
+    const mappedView = viewExt.map(({ n }) => ({ n: n + 1 }))
+    expect(mappedView.readAllData()).toEqual([
+      { n: 1 },
+      { n: 2 },
+      { n: 3 },
+    ])
+
+    table.retract({ n: 2 })
+    expect(mappedView.readAllData()).toEqual([
+      { n: 1 },
+      { n: 2 },
+    ])
+  })
+})
+
+describe("Reduce", () => {
+  test("Reduces simple cases", () => {
+    const table = datalog.intoTable([
+      { n: 0 },
+      { n: 1 },
+      { n: 2 },
+    ])
+    const view = table.view()
+    const viewExt = new ViewExt.Impl(view)
+    const reducedView = viewExt.reduce((acc, { kind, datum: { n } }) => {
+      switch (kind) {
+        case datalog.Added:
+          return acc + n
+        case datalog.Removed:
+          return acc - n
+        case datalog.Modified:
+          throw new Error("Wasn't expected a modification")
+      }
+    }, 0)
+
+    expect(reducedView.readAllData()).toEqual([
+      3,
+    ])
+
+    table.retract({ n: 2 })
+    expect(reducedView.recentData()).toEqual([
+      { kind: datalog.Modified, datum: 1, oldDatum: 3 }
+    ])
+  })
+})
+
+
+describe("SortBy", () => {
+  test("Refresh datalog.gallop memory", () => {
+    expect(datalog.gallop([0, 1, 2, 4], (n: number) => n < 3)).toBe(3)
+    expect(datalog.gallop([0, 1, 2, 4], (n: number) => n < -1)).toBe(0)
+    expect(datalog.gallop([0, 1, 2, 4], (n: number) => n < 5)).toBe(4)
+    expect(datalog.gallop([0, 1, 2, 4], (n: number) => n < 2)).toBe(2)
+  })
+
+  test("Sort tuple on Objects", () => {
+    expect(datalog.sortTuple({ a: 1 }, { a: 1, b: 2 })).toBe(0)
+    expect(datalog.sortTuple({ a: 1 }, { a: 2, b: 2 })).toBe(0)
+  })
+
+  test("Simple Sort", () => {
+    const table = datalog.intoTable([
+      { n: 1 },
+      { n: 6 },
+      { n: 2 },
+      { n: 5 },
+      { n: 0 },
+    ])
+
+    const view = table.view()
+    const viewExt = new ViewExt.Impl(view)
+    const sortedView = viewExt.sortBy(({ n: a }, { n: b }) => a < b ? -1 : a > b ? 1 : 0)
+
+    expect(sortedView.readAllData()).toEqual([
+      { datum: { n: 0 }, index: 0 },
+      { datum: { n: 1 }, index: 1 },
+      { datum: { n: 2 }, index: 2 },
+      { datum: { n: 5 }, index: 3 },
+      { datum: { n: 6 }, index: 4 },
+    ])
+
+    table.assert({ n: 3 })
+    expect(sortedView.recentData()).toEqual(
+      [
+        { kind: Added, datum: { index: 3, datum: { n: 3 } } },
+        {
+          kind: Modified,
+          datum: { index: 4, datum: { n: 5 } },
+          oldDatum: { index: 3, datum: { n: 5 } }
+        },
+        {
+          kind: Modified,
+          datum: { index: 5, datum: { n: 6 } },
+          oldDatum: { index: 4, datum: { n: 6 } }
+        }
+      ]
+    )
+
+    table.retract({ n: 2 })
+    expect(sortedView.recentData()).toEqual(
+      [
+        { kind: Removed, datum: { index: 2, datum: { n: 2 } } },
+        {
+          kind: Modified,
+          datum: { index: 2, datum: { n: 3 } },
+          oldDatum: { index: 3, datum: { n: 3 } }
+        },
+        {
+          kind: Modified,
+          datum: { index: 3, datum: { n: 5 } },
+          oldDatum: { index: 4, datum: { n: 5 } }
+        },
+        {
+          kind: Modified,
+          datum: { index: 4, datum: { n: 6 } },
+          oldDatum: { index: 5, datum: { n: 6 } }
+        }
+      ]
+    )
+  })
+
+  test("Take from sorted", () => {
+    const table = datalog.intoTable([
+      { n: 1 },
+      { n: 6 },
+      { n: 2 },
+      { n: 5 },
+      { n: 0 },
+    ])
+
+    const view = table.view()
+    const viewExt = new ViewExt.Impl(view)
+    const sortedView = viewExt.sortBy(({ n: a }, { n: b }) => a < b ? -1 : a > b ? 1 : 0)
+    const takedView = sortedView.take(3)
+
+    expect(takedView.readAllData()).toEqual([
+      { datum: { n: 0 }, index: 0 },
+      { datum: { n: 1 }, index: 1 },
+      { datum: { n: 2 }, index: 2 },
+    ])
+
+    const dropedView = sortedView.drop(2)
+
+    expect(dropedView.readAllData()).toEqual([
+      { datum: { n: 2 }, index: 2 },
+      { datum: { n: 5 }, index: 3 },
+      { datum: { n: 6 }, index: 4 },
+    ])
+
+    const dropedViewAgain = dropedView.drop(1)
+
+    expect(dropedViewAgain.readAllData()).toEqual([
+      { datum: { n: 5 }, index: 3 },
+      { datum: { n: 6 }, index: 4 },
+    ])
+
+    const takedView2 = dropedViewAgain.take(1)
+
+    expect(takedView2.readAllData()).toEqual([
+      { datum: { n: 5 }, index: 3 },
+    ])
+    const dropped3 = takedView.drop(1)
+
+    expect(dropped3.readAllData()).toEqual([
+      { datum: { n: 1 }, index: 1 },
+      { datum: { n: 2 }, index: 2 },
+    ])
+
+    table.retract({ n: 2 })
+
+    expect(dropped3.readAllData()).toEqual([
+      { datum: { n: 1 }, index: 1 },
+      { datum: { n: 5 }, index: 2 },
+    ])
+  })
+  test("OrderBy", () => {
+    const table = datalog.intoTable([
+      { n: 1 },
+      { n: 6 },
+      { n: 2 },
+      { n: 5 },
+      { n: 0 },
+    ])
+
+    const view = table.view()
+    const viewExt = new ViewExt.Impl(view)
+    const sortedView = viewExt.orderBy('n', true)
+
+    expect(sortedView.readAllData()).toEqual([
+      { datum: { n: 0 }, index: 0 },
+      { datum: { n: 1 }, index: 1 },
+      { datum: { n: 2 }, index: 2 },
+      { datum: { n: 5 }, index: 3 },
+      { datum: { n: 6 }, index: 4 },
+    ])
+
+    const sortedOppositeView = viewExt.orderBy('n', false)
+
+    expect(sortedView.readAllData()).toEqual([
+      { datum: { n: 6 }, index: 0 },
+      { datum: { n: 5 }, index: 1 },
+      { datum: { n: 2 }, index: 2 },
+      { datum: { n: 1 }, index: 3 },
+      { datum: { n: 0 }, index: 4 },
+    ])
   })
 })
--- src/view-ext.ts
+++ src/view-ext.ts
@@ -1,12 +1,11 @@
 import { View, RecentDatum } from './datalog'
 import * as datalog from './datalog'
 
-type UndoEffectFn = () => void
-
 /**
- * A function that does some effect, and returns an callback to undo that effect
+ * A function that does some effect. It's up to you to undo that effect when you
+ * get a Removed kind of RecentDatum
  */
-type EffectFn<T> = (t: T) => UndoEffectFn
+type EffectFn<T> = (t: RecentDatum<T>) => void
 
 type Indexed<T> = { index: number, datum: T }
 
@@ -14,20 +13,193 @@ interface ViewExt<T> extends datalog.View<T> {
   map<O>(f: (t: T) => O): ViewExt<O>;
   mapEffect<F extends EffectFn<T>>(f: F): void;
 
-  // reduce<Acc>(reducer: (accumulator: Acc, recentVal: datalog.RecentDatum<T>) => Acc): ViewExt<Acc>
+  reduce<Acc>(reducer: (accumulator: Acc, recentVal: datalog.RecentDatum<T>) => Acc, initialVal: Acc): ViewExt<Acc>
 
-  // sortBy(sortFn: (a: T, B: T) => -1 | 0 | 1): ViewExt<Indexed<T>>
-  // orderBy(key: string, ascending?: boolean): ViewExt<Indexed<T>>
+  sortBy(sortFn: (a: T, b: T) => -1 | 0 | 1): IndexedViewExt<T>
+  orderBy(key: keyof T, ascending?: boolean): IndexedViewExt<T>
+}
 
-  // take(n: number): ViewExt<T>
-  // drop(n: number): ViewExt<T>
+interface IndexedViewExt<T> extends ViewExt<Indexed<T>> {
+  take(n: number): IndexedViewExt<T> & ViewExt<Indexed<T>>
+  drop(n: number): IndexedViewExt<T> & ViewExt<Indexed<T>>
+}
+
+class SingleItemView<T> implements datalog.View<T> {
+  lastValSincePoll: T
+  currentVal: T
+  subscribers: Array<() => void> = []
+  newDatumSubscribers: Array<(d: RecentDatum<T>) => void> = []
+  modifiedSinceLastPolled = false
+
+  constructor(initialVal: T) {
+    this.currentVal = initialVal
+    this.lastValSincePoll = initialVal
+  }
+
+  copy(): View<T> {
+    const copied = new SingleItemView(this.currentVal)
+    copied.lastValSincePoll = this.lastValSincePoll
+
+    return copied
+  }
+
+  _setValue(v: T) {
+    this.modifiedSinceLastPolled = true
+    this.subscribers.map(s => s())
+    this.currentVal = v
+    this.newDatumSubscribers.forEach(subscriber => subscriber({ kind: datalog.Modified, datum: v, oldDatum: this.lastValSincePoll }))
+  }
+
+  recentData(): null | Array<RecentDatum<T>> {
+    const lastVal = this.lastValSincePoll
+    const modifiedSinceLastPolled = this.modifiedSinceLastPolled
+    // Reset vals
+    this.modifiedSinceLastPolled = false
+    this.lastValSincePoll = this.currentVal
+
+    return modifiedSinceLastPolled ? [{ kind: datalog.Modified, datum: this.currentVal, oldDatum: lastVal }] : null
+  }
+
+  readAllData(): Array<T> {
+    this.modifiedSinceLastPolled = false
+    this.lastValSincePoll = this.currentVal
+    return [this.currentVal]
+  }
+
+  onChange(subscriber: () => void): () => void {
+    this.subscribers.push(subscriber)
+    return () => this.subscribers = this.subscribers.filter(s => s !== subscriber)
+  }
+
+  onNewDatum(subscriber: (d: RecentDatum<T>) => void): () => void {
+    this.newDatumSubscribers.push(subscriber)
+    return () => this.newDatumSubscribers = this.newDatumSubscribers.filter(s => s !== subscriber)
+  }
 }
 
 function isPlainObj(o: any): boolean {
   return typeof o == 'object' && o.constructor == Object;
 }
 
-class ViewExtImpl<T> implements ViewExt<T> {
+class SortedView<T> implements View<Indexed<T>> {
+  innerView: View<T>
+  sorted: Array<T> = []
+  changes: Array<RecentDatum<Indexed<T>>> = []
+  subscribers: Array<() => void> = []
+  newDatumSubscribers: Array<(d: RecentDatum<Indexed<T>>) => void> = []
+  sortFn: (a: T, b: T) => -1 | 0 | 1
+
+  constructor(fromView: View<T>, sortFn: (a: T, b: T) => -1 | 0 | 1) {
+    this.innerView = fromView
+    this.sorted = this.innerView.readAllData().sort(sortFn)
+    this.sortFn = sortFn
+
+    const onChange = ({ kind, datum }: RecentDatum<T>) => {
+      const scopeChanges: Array<RecentDatum<Indexed<T>>> = []
+      // const stableRelation = innerVar.stable.relations[0]
+      // if (!stableRelation || stableRelation.elements.length === 0) {
+      if (this.sorted.length === 0) {
+        if (kind !== datalog.Added) {
+          throw new Error("Unexpected! We don't have anything to remove or modify")
+        }
+
+        this.changes.push({ kind: datalog.Added, datum: { index: 0, datum } })
+        this.sorted.push(datum)
+        return
+      }
+
+      switch (kind) {
+        case datalog.Added:
+          // A new datum was added, let's find out where it goes in the list
+          // const positionToInsert = datalog.gallop(elements, (tuple) => sortFn(toObj(tuple), datum) === -1)
+          const positionToInsert = datalog.gallop(this.sorted, (d) => sortFn(d, datum) === -1)
+          this.sorted.splice(positionToInsert, 0, datum)
+          scopeChanges.push({ kind: datalog.Added, datum: { index: positionToInsert, datum } })
+          for (let i = positionToInsert + 1; i < this.sorted.length; i++) {
+            scopeChanges.push({
+              kind: datalog.Modified,
+              datum: { index: i, datum: this.sorted[i] },
+              oldDatum: { index: i - 1, datum: this.sorted[i] }
+            })
+          }
+          break;
+        case datalog.Removed:
+          // A new datum was removed, let's find out where it was in the list
+          const positionToRemove = datalog.gallop(this.sorted, (d) => sortFn(d, datum) === -1)
+          // Check if the item is there
+          if (datalog.sortTuple(this.sorted[positionToRemove], datum) !== 0) {
+            throw new Error("Tried to remove a value that doesn't exist!!")
+          }
+          // Remove from our array
+          this.sorted.splice(positionToRemove, 1)
+          scopeChanges.push({ kind: datalog.Removed, datum: { index: positionToRemove, datum } })
+
+          for (let i = positionToRemove; i < this.sorted.length; i++) {
+            scopeChanges.push({
+              kind: datalog.Modified,
+              datum: { index: i, datum: this.sorted[i] },
+              oldDatum: { index: i + 1, datum: this.sorted[i] }
+            })
+          }
+          break;
+        case datalog.Modified:
+          throw new Error("Not implemented! – Modification datum on indexed View")
+        // Tricky case, we have to see where the item was, and where it should
+        // go. I don't think it's possible unless we add a new field to
+        // modified... (old value)
+      }
+
+      this.newDatumSubscribers.forEach(subscriber => {
+        scopeChanges.forEach(change => subscriber(change))
+      })
+      this.changes = this.changes.concat(scopeChanges)
+    }
+
+    this.innerView.onChange(() => {
+      this.innerView.recentData()?.map(onChange)
+    })
+    this.innerView.recentData()?.map(onChange)
+
+
+    let subscribers: Array<() => void> = []
+
+
+  }
+  recentData() {
+    if (this.changes.length > 0) {
+      const recentChanges = this.changes
+      this.changes = []
+      return recentChanges
+    }
+    return null
+  }
+
+  readAllData() {
+    return this.sorted.map((datum, index) => ({ index, datum }))
+  }
+
+  onChange(f: () => void) {
+    this.subscribers.push(f)
+    return () => { this.subscribers = this.subscribers.filter(s => s !== f) }
+  }
+
+  onNewDatum(f: (d: RecentDatum<Indexed<T>>) => void) {
+    this.newDatumSubscribers.push(f)
+    return () => {
+      this.newDatumSubscribers = this.newDatumSubscribers.filter(subscriber => {
+        subscriber !== f
+      })
+    }
+  }
+
+  copy() {
+    const copied = new SortedView(this.innerView, this.sortFn)
+    return copied
+  }
+}
+
+
+export class Impl<T> implements ViewExt<T> {
   innerView: View<T>
   constructor(v: View<T>) {
     this.innerView = v
@@ -39,47 +211,144 @@ class ViewExtImpl<T> implements ViewExt<T> {
     return this.innerView.readAllData()
   }
 
-  // Returns unsubscribe fn
-  onChange(subscriber: (t: T, isRetraction: boolean) => void): () => void {
+  copy(): View<T> {
+    return this.innerView.copy()
+  }
+
+  onChange(subscriber: () => void): () => void {
     return this.innerView.onChange(subscriber)
   }
 
-  onChangeRecentDatum(subscriber: (t: datalog.RecentDatum<T>) => void): () => void {
-    return this.innerView.onChangeRecentDatum(subscriber)
+  onNewDatum(subscriber: (d: RecentDatum<T>) => void): () => void {
+    return this.innerView.onNewDatum(subscriber)
   }
 
   map<O>(f: (t: T) => O): ViewExt<O> {
     const out = datalog._newTable<O>()
-    this.innerView.onChange((v, isRetraction) => {
-      if (!isRetraction) {
-        out.assert(f(v))
+    const onChange = ({ kind, datum }: RecentDatum<T>) => {
+      if (kind === datalog.Added) {
+        out.assert(f(datum))
       } else {
-        out.retract(f(v))
+        out.retract(f(datum))
       }
+    }
+    this.innerView.onChange(() => {
+      this.innerView.recentData()?.map(onChange)
     })
+    this.innerView.recentData()?.map(onChange)
 
-    return new ViewExtImpl(out.view())
-  }
-
-
-  warnMissingUndo() {
-    console.warn("Failed to lookup Undo FN. Is this value serializable?")
+    return new Impl(out.view())
   }
 
   mapEffect<F extends EffectFn<T>>(f: F) {
-    const out = datalog._newTable<typeof datalog.EmptyObj>()
-    const fnLookup: any = {}
-    this.innerView.onChange((v, isRetraction) => {
-      const key = isPlainObj(v) ? JSON.stringify(v) : v
-      if (!isRetraction) {
-        const undoFn = f(v)
-        fnLookup[key] = undoFn
-      } else {
-        const undoFn = fnLookup[key] || this.warnMissingUndo
-        undoFn()
-      }
+    const onChange = (r: RecentDatum<T>) => {
+      f(r)
+    }
+    this.innerView.onChange(() => {
+      this.innerView.recentData()?.map(onChange)
     })
+    this.innerView.recentData()?.map(onChange)
   }
 
+  reduce<Acc>(reducer: (accumulator: Acc, recentVal: datalog.RecentDatum<T>) => Acc, initalVal: Acc): ViewExt<Acc> {
+    let acc = new SingleItemView(initalVal)
+    const onChange = (r: RecentDatum<T>) => {
+      acc._setValue(reducer(acc.currentVal, r))
+    }
+    this.innerView.onChange(() => {
+      this.innerView.recentData()?.map(onChange)
+    })
+    this.innerView.recentData()?.map(onChange)
+    return new Impl(acc)
+  }
 
+  sortBy(sortFn: (a: T, b: T) => -1 | 0 | 1): IndexedViewExt<T> {
+    return new IndexedImpl(new SortedView(this.innerView, sortFn))
+  }
+
+  orderBy(key: keyof T, ascending: boolean = true): IndexedViewExt<T> {
+    const sortFn = (a: T, b: T): -1 | 0 | 1 => {
+      const aEl = a[key]
+      const bEl = b[key]
+      const result = (aEl < bEl ? -1 : aEl > bEl ? 1 : 0)
+      return result * (ascending ? 1 : -1) as -1 | 0 | 1
+    }
+    return this.sortBy(sortFn)
+  }
+}
+
+export class IndexedImpl<T> implements IndexedViewExt<T> {
+  innerView: ViewExt<Indexed<T>>
+  filters: { take?: number, drop?: number } = {}
+  constructor(fromView: View<Indexed<T>>) {
+    this.innerView = new Impl(fromView)
+  }
+
+  take(n: number): IndexedViewExt<T> {
+    const nextView = this.copy()
+    nextView.filters.take = n
+    nextView.filters.drop = this.filters.drop
+    return nextView
+  }
+
+  drop(n: number): IndexedViewExt<T> {
+    const nextView = this.copy()
+    nextView.filters.drop = (this.filters.drop || 0) + n
+    nextView.filters.take = this.filters.take ? Math.max(this.filters.take - n, 0) : this.filters.take
+    return nextView
+  }
+
+  private passThruDatum(datum: Indexed<T>): boolean {
+    if (!this.filters.take && !this.filters.drop) {
+      return true
+    }
+
+    return (datum.index >= (this.filters.drop || 0) && datum.index < ((this.filters.take || Infinity) + (this.filters.drop || 0)))
+  }
+
+  recentData(): null | Array<RecentDatum<Indexed<T>>> {
+    return this.innerView.recentData()?.filter((recentDatum) => {
+      if (recentDatum.kind === datalog.Modified) {
+        return this.passThruDatum(recentDatum.datum) || this.passThruDatum(recentDatum.oldDatum)
+      }
+      return this.passThruDatum(recentDatum.datum)
+    }) || null
+  }
+
+  readAllData(): Array<Indexed<T>> {
+    return this.innerView.readAllData().filter((datum) => this.passThruDatum(datum))
+  }
+  // Returns unsubscribe fn
+  onChange(subscriber: () => void): () => void {
+    return this.innerView.onChange(subscriber)
+  }
+
+  // Returns unsubscribe fn
+  onNewDatum(subscriber: (d: RecentDatum<Indexed<T>>) => void): () => void {
+    return this.innerView.onNewDatum(subscriber)
+  }
+
+  copy() {
+    return new IndexedImpl(this.innerView.copy())
+  }
+
+  map<O>(f: (t: Indexed<T>) => O): ViewExt<O> {
+    return this.innerView.map(f)
+  }
+
+  mapEffect<F extends EffectFn<Indexed<T>>>(f: F): void {
+    return this.innerView.mapEffect(f)
+  }
+
+  reduce<Acc>(reducer: (accumulator: Acc, recentVal: datalog.RecentDatum<Indexed<T>>) => Acc, initialVal: Acc): ViewExt<Acc> {
+    return this.innerView.reduce(reducer, initialVal)
+  }
+
+  sortBy(sortFn: (a: Indexed<T>, b: Indexed<T>) => -1 | 0 | 1): IndexedViewExt<Indexed<T>> {
+    return this.innerView.sortBy(sortFn)
+  }
+
+  orderBy(key: keyof Indexed<T>, ascending: boolean = true): IndexedViewExt<Indexed<T>> {
+    return this.innerView.orderBy(key, ascending)
+  }
 }
